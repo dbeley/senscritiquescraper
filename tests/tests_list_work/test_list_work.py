21e36807c0eab913fada5f25bb8e458c19aaa984
dbeley <6568955+dbeley@users.noreply.github.com>
2020-07-05 22:18:35 +0200

    Update tests.

--- tests/tests_list_work/test_list_work.py
+++ tests/tests_list_work/test_list_work.py
@@ -4,5 +4,5 @@ from senscritiquescraper.utils import list_work_utils
 def test_get_rows_from_list_work(list_work_soup):
     rows = list_work_utils.get_rows_from_list_work(list_work_soup)
     print(len(rows))
-    if len(rows) != 17:
+    if len(rows) != 26:
         raise AssertionError()
